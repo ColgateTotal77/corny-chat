425000efe3638166890fc494e9a48f4b5b5b80d8
snazarenko <serega2005n@gmail.com>
2024-12-09 22:53:59 +0200

    fix getting history

--- client/src/chat_visual/gtk_src/GTK_Start/on_scroll_changed.c
+++ client/src/chat_visual/gtk_src/GTK_Start/on_scroll_changed.c
@@ -17,10 +17,10 @@ void on_scroll_changed(GtkAdjustment *adjustment, gpointer user_data) {
         }
     }
     else if (value <= lower) {
-        if(scroll_data->chat->is_group) {
+        if(scroll_data->chat->is_group && scroll_data->chat->last_message_id) {
             get_num_of_msgs_from_group(scroll_data->ssl, scroll_data->chat->contact_id, scroll_data->chat->last_message_id, 15);
         }
-        else {
+        else if (scroll_data->chat->last_message_id){
             get_num_of_msgs_with_user(scroll_data->ssl, scroll_data->chat->contact_id, scroll_data->chat->last_message_id, 15);
         }
     }
